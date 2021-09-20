6b5789a104777d9c6b10b038ce828311b3f9233b
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-20 15:43:56 +0900

    Food一覧表示機能の作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 User.create!(email: "test@example.com", password: "password")
+user1 = User.first
+Food.create!(name: "パンケーキ", comment: "ふわふわです！", user_id: user1.id)
+Food.create!(name: "カレー", comment: "スパイシーです！", user_id: user1.id)
+Food.create!(name: "ラーメン", comment: "好物です！", user_id: user1.id)
 puts "ユーザーの初期データインポートに成功しました。"
