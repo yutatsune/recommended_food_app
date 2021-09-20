16721c60d1d3edd5027b436a7e39f21ca0b40103
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-20 21:43:34 +0900

    Food一覧表示・詳細表示の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
-User.create!(email: "test@example.com", password: "password")
-user1 = User.first
-Food.create!(name: "パンケーキ", comment: "ふわふわです！", user_id: user1.id)
-Food.create!(name: "カレー", comment: "スパイシーです！", user_id: user1.id)
-Food.create!(name: "ラーメン", comment: "好物です！", user_id: user1.id)
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+
+user1 = User.create!(email: "test@example.com", password: "password")
+user2 = User.create!(email: "test2@example.com", password: "password")
+user3 = User.create!(email: "test3@example.com", password: "password")
+
+user1.foods.create!(name: "パンケーキ", comment: "ふわふわです！\nシロップをかけたい！")
+user2.foods.create!(name: "カレー", comment: "スパイシーです！\n食欲をそそる！")
+user3.foods.create!(name: "ラーメン", comment: "好物です！\n味玉もつけたい！")
+user2.foods.create!(name: "チャーハン", comment: "香ばしくて美味しい！\n餃子も一緒に食べたい！")
+user1.foods.create!(name: "ショートケーキ", comment: "甘くて美味しい！\nたまには食べたい！")
+
 puts "ユーザーの初期データインポートに成功しました。"
