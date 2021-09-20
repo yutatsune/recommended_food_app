5414177cdcef2e69fb9c009787ce4ac4dfedb05e
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-20 16:02:24 +0900

    Food新規投稿機能の追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,20 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @foods = Food.includes(:user)
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    food = current_user.foods.create!(food_params)
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :comment)
   end
 end
