00eedd7b62682b5a6edba5653c2809f20f42952b
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-22 13:12:49 +0900

    Likeモデルのバリデーション・アソシエーション追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,7 @@
 class Food < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :food
+  validates :user_id, uniqueness: {
+    scope: :food_id,
+    message: "は同じ投稿に2回以上いいねはできません"
+  }
 end
