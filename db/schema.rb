243b819163347448183e7d7fd83cc3e2d1d22c5f
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-22 12:59:58 +0900

    Likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_081331) do
+ActiveRecord::Schema.define(version: 2021_09_22_035751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2021_09_21_081331) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_likes_on_food_id"
+    t.index ["user_id", "food_id"], name: "index_likes_on_user_id_and_food_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,4 +45,6 @@ ActiveRecord::Schema.define(version: 2021_09_21_081331) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "likes", "foods"
+  add_foreign_key "likes", "users"
 end
